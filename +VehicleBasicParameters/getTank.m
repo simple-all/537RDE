0fef01b4c9258dcfefa6e1eaff861a6094e988a5
mcclaskey14 <33603889+mcclaskey14@users.noreply.github.com>
2017-11-29 20:39:11 -0500

    Fixed error in units

--- +VehicleBasicParameters/getTank.m
+++ +VehicleBasicParameters/getTank.m
@@ -1,8 +1,8 @@
 %% Calculates the weight and volume of a pill style tank given...
-% 1. Normal Operating Pressure (P_operating) [MPa]
+% 1. Normal Operating Pressure (P_operating) [psi]
 % 2. Diameter of the tank (d) [inches]
 % 3. Overall length of tank (l)
-% 4. Fluid Density (fluid_density) [lbm/ft3]
+% 4. Fluid Density (fluid_density) [lb/ft3]
 
 function [Weight_tank, Volume_tank, Weight_fluid, Surface_Area] = getTank(P_operating, d, l, fluid_density)
 r = d/2; %[in]
@@ -10,5 +10,5 @@ a = l - (2*d); %[in]
 Surface_Area = 2*pi*r*(2*r + a); %[sq.in]
 Volume_tank = pi*r^2*((4/3)*r+a); %[cu.in]
 Weight_tank = (3*P_operating*Volume_tank)/(1*10^6); %[lbf] %Burst pressure is 3 times max normal operating
-Weight_fluid = Volume_tank * (fluid_density/1728); %[lbm]
+Weight_fluid = Volume_tank * (fluid_density/1728); %[lbf]
 end
